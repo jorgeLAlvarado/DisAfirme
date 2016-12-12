976e519a59f13f7c990fc2d03dc2205fab1dc4de
BayronGamboa <bayron.gamboa@eisei.net.mx>
2016-12-12 16:52:38 -0600

    clases para login exitoso reutilisados en User DAO

--- DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/UserDao.java
+++ DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/UserDao.java
@@ -8,4 +8,29 @@ public interface UserDao {
 	
 	public boolean actualizarCorreoLogin(CorreoElectronicoDTO correoElectronicoDTO) throws Exception;
 	
+	// daos de login exitoso
+	// +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+//
+	public String obtenerTipoRegimen(String contrato);
+
+	public int obtenerNumeroIntento(String contrato);
+
+	public boolean verificarUsuarioRegistrado(String contrato);
+
+	public boolean registrarUsuarioAlias(String contrato);
+
+	public boolean actualizarPrimerLoginConAlias(String contrato);
+
+	public boolean incrementarNumeroIntentos(String contrato);
+
+	public boolean actualizarAlias(String contrato, String alias);
+
+	boolean actualizarAliasLogin(String contrato, String alias, String avatar);
+
+	public String obtenerAvatar(String contrato);
+
+	// Clase de validar AVATAr modificar cuando este ese modulo
+	public boolean registraCambioAlias(AliasAvatarDTO aliasAvatarDTO) throws Exception;
+
+	public String obtenerFechaNacimiento(String cliente);
+	// +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+//
 }
