74127efd50a1fe93e3f19f6ca8ff3ae3ab36d382
SeleneMena <mayra.mena@eisei.net.mx>
2016-12-14 18:37:14 -0600

    DELATE

--- DisService/src/main/java/com/afirme/afirmenet/empresas/dao/token/TokenDao.java
+++
@@ -1,9 +0,0 @@
-package com.afirme.afirmenet.empresas.dao.token;
-import java.util.List;
-
-import com.afirme.afirmenet.model.token.Token;
-
-public interface TokenDao {
-
-	List<Token> getTokens(Boolean soloDisponibles);
-}
