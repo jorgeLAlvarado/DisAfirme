6212331bf9165f999160b2e2427c9c57342e558d
jorgeLAlvarado <jorge.alvarado@eisei.net.mx>
2016-12-26 15:35:58 -0600

    SS

--- DisService/src/main/java/com/afirme/afirmenet/dao/impl/transferencia/TransferenciaDaoImpl.java
+++ DisService/src/main/java/com/afirme/afirmenet/dao/impl/transferencia/TransferenciaDaoImpl.java
@@ -1,24 +1,13 @@
 package com.afirme.afirmenet.dao.impl.transferencia;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.Map;
-
-import org.joda.time.DateTime;
-import com.afirme.afirmenet.utils.AfirmeNetLog;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 
-import com.afirme.afirmenet.dao.AS400Dao;
-import com.afirme.afirmenet.dao.DB2Dao;
 import com.afirme.afirmenet.dao.transferencia.TransferenciaDao;
 import com.afirme.afirmenet.enums.ConfigPersonas;
 import com.afirme.afirmenet.ibs.databeans.GRPUSR;
 import com.afirme.afirmenet.model.transferencia.DomingoElectronico;
 import com.afirme.afirmenet.utils.AfirmeNetConstants;
-import com.afirme.afirmenet.utils.time.TimeUtils;
+import com.afirme.afirmenet.utils.AfirmeNetLog;
 
 @Repository
 public class TransferenciaDaoImpl implements TransferenciaDao {
