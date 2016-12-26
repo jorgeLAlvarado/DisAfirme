789cb64804ab7023a1bb4143b3d735790a17d942
SeleneMena <mayra.mena@eisei.net.mx>
2016-12-26 17:54:16 -0600

    55

--- DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/OTPDao.java
+++ DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/OTPDao.java
@@ -8,13 +8,39 @@ package com.afirme.afirmenet.empresas.dao.acceso;
  */
 public interface OTPDao {
 	
+	/**
+	 * @param contrato
+	 * @return
+	 */
 	public String obtenToken(String contrato);
 
+	/**
+	 * @param contrato
+	 * @return
+	 */
 	public boolean usaTokens(String contrato);
 
+	/**
+	 * @param contrato
+	 * @param usuario
+	 * @param serialToken
+	 * @param codigoActivacion
+	 * @return
+	 */
 	public boolean validaTokenXActivar(String contrato, String usuario, int serialToken, String codigoActivacion);
 
+	/**
+	 * @param contrato
+	 * @param usuario
+	 * @param codigoSegEncrypt
+	 * @return
+	 */
 	public boolean setCodigoSeguridad(String contrato, String usuario, String codigoSegEncrypt);
 
+	/**
+	 * @param serialToken
+	 * @param fechaVencimiento
+	 * @return
+	 */
 	public boolean setFechaVencimiento(String serialToken, String fechaVencimiento);
 }
--- DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/PasswordDao.java
+++ DisService/src/main/java/com/afirme/afirmenet/empresas/dao/acceso/PasswordDao.java
@@ -10,13 +10,37 @@ import java.util.Map;
  */
 public interface PasswordDao {
 	
+	/**
+	 * @param idContrato
+	 * @param password
+	 * @param nuevoPassword
+	 * @return
+	 */
 	public Map<String, String> updatePassword(String idContrato, String password, String nuevoPassword);
 
+	/**
+	 * @param idContrato
+	 * @param password
+	 * @return
+	 */
 	public boolean setPassword(String idContrato, String password);
 
+	/**
+	 * @param idContrato
+	 */
 	public void mailNotificacionCambioPwd(String idContrato);
 
+	/**
+	 * @param idContrato
+	 * @param nuevoPassword
+	 * @return
+	 */
 	public Map<String, String> solicitudCambioPwd(String idContrato, String nuevoPassword);
 
+	/**
+	 * @param idContrato
+	 * @param codigoToken
+	 * @return
+	 */
 	public String validaPassword(String idContrato, String codigoToken);
 }
