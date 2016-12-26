86887603ab966bdff37f4fbf59face0689668ffd
SeleneMena <mayra.mena@eisei.net.mx>
2016-12-26 17:20:01 -0600

    dao

--- DisService/src/main/java/com/afirme/afirmenet/dao/consultas/HistorialDao.java
+++ DisService/src/main/java/com/afirme/afirmenet/dao/consultas/HistorialDao.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 import com.afirme.afirmenet.ibs.beans.consultas.TipoTransaccion;
 
+/**
+ * @author Jorge Luis Alvarado
+ * @version 1.0.0
+ * Created on Created on Dic 10, 2016 3:50:05 PM by Jorge
+ */
 public interface HistorialDao {
+	/**
+	 * @param esBasicoSinToken
+	 * @return
+	 */
 	List<TipoTransaccion> listaTransacciones(boolean esBasicoSinToken);
+	/**
+	 * @param esBasicoSinToken
+	 * @return
+	 */
 	List<String> categorias(boolean esBasicoSinToken);
 }
