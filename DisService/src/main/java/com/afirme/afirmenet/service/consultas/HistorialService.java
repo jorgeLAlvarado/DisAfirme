41ea3420ef8b5db4e9c5cc78ad01961136081057
jorgeLAlvarado <jorge.alvarado@eisei.net.mx>
2016-12-26 18:53:12 -0600

    ss

--- DisService/src/main/java/com/afirme/afirmenet/service/consultas/HistorialService.java
+++ DisService/src/main/java/com/afirme/afirmenet/service/consultas/HistorialService.java
@@ -12,7 +12,21 @@ import com.afirme.afirmenet.model.transferencia.TransferenciaBase;
  *
  */
 public interface HistorialService {
+	/**
+	 * Obtener lista de transacciones
+	 * @param esBasicoSinToken
+	 * @return
+	 */
 	List<TipoTransaccion> listaTransacciones(boolean esBasicoSinToken);
+	/**
+	 * Obtener la categoria de la transaccion
+	 * @param esBasicoSinToken
+	 * @return
+	 */
 	List<String> categorias(boolean esBasicoSinToken);
+	/**
+	 * Obtener la informacion faltante para el comprobante
+	 * @param comprobante
+	 */
 	void obtenerInformacionExtra(TransferenciaBase comprobante);
 }
