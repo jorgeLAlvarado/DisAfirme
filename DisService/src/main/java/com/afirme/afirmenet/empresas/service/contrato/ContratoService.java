99c4a3b8318f7a597fbfa5ea1cca9381fec2392b
SeleneMena <mayra.mena@eisei.net.mx>
2016-12-27 10:23:51 -0600

    v

--- DisService/src/main/java/com/afirme/afirmenet/empresas/service/contrato/ContratoService.java
+++ DisService/src/main/java/com/afirme/afirmenet/empresas/service/contrato/ContratoService.java
@@ -5,14 +5,54 @@ import java.sql.SQLException;
 
 import com.afirme.afirmenet.beas.login.Contrato;
 import com.afirme.afirmenet.beas.login.JBLogin;
-
+/**
+ * clase para validar la contraseña
+ *  * 
+ * @author Bayron Gamboa Martinez
+ *
+ * @version 1.0.0
+ */
 public interface ContratoService {
 
+	/**
+	 * @param idContrato
+	 * @return
+	 */
 	public Contrato getDatosContrato(String idContrato);
+	/**
+	 * @param idContrato
+	 * @return
+	 * @throws SQLException
+	 */
+	/**
+	 * @param idContrato
+	 * @return
+	 * @throws SQLException
+	 */
 	public JBLogin getDatosLogIn(String idContrato) throws SQLException;
+	/**
+	 * @param idContrato
+	 * @param status
+	 * @return
+	 */
 	public boolean setStatus(String idContrato, String status);
+	/**
+	 * @param contrato
+	 * @param idContrato
+	 * @return
+	 */
 	public boolean updateContrato(Contrato contrato, String idContrato);
+	/**
+	 * @param idContrato
+	 * @param password
+	 * @param status
+	 * @return
+	 */
 	public boolean setPassword(String idContrato, String password, String status);
+	/**
+	 * @param contrato
+	 * @return
+	 */
 	public String getCuentaContrato(String contrato);
 	/* TODO: JBEntity aún tiene métodos por implementar. Agregar conforme se vayan necesitando.*/
 }
