8582451505257fea60f8ace99f59b79de32fc8b5
SeleneMena <mayra.mena@eisei.net.mx>
2016-12-27 10:09:48 -0600

    ll

--- DisService/src/main/java/com/afirme/afirmenet/empresas/service/transferencia/ComprobanteTransferenciaService.java
+++ DisService/src/main/java/com/afirme/afirmenet/empresas/service/transferencia/ComprobanteTransferenciaService.java
@@ -2,8 +2,15 @@ package com.afirme.afirmenet.empresas.service.transferencia;
 
 import com.afirme.afirmenet.model.transferencia.Comprobante;
 
+/**
+ * @author Usuario
+ *
+ */
 public interface ComprobanteTransferenciaService {
 	
+	/**
+	 * @param comprobante
+	 */
 	public void insertaConfirmacionOperacion(Comprobante comprobante);
 
 }
